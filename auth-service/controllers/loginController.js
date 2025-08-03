838c0288cf805b4c4a1163baaf51cbb2213ef915
IssaWick <ipiyasara@gmail.com>
2025-08-03 14:08:20 +0530

    modified login controller

--- auth-service/controllers/loginController.js
+++ auth-service/controllers/loginController.js
@@ -59,7 +59,8 @@ export const login = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: false, // Set to true in production with HTTPS
+        secure: false, // Set to true in production with 
+        sameSite: 'Lax',
         maxAge: 2 * 60 * 60 * 1000
       })
       .status(200)
