d642bbb32479ec126e2c3fe2b76a8f50fbf6be7b
IssaWick <ipiyasara@gmail.com>
2025-08-03 09:44:53 +0530

    update profile server file with cors

--- profile-service/server.js
+++ profile-service/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 import profileRoutes from './routes/profileRoute.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
