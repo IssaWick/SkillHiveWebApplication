9d6491f7fc06acdd9aa9cd4b914ebe5959e0d931
yashinikaa <rl.yashinikavidushani@gmail.com>
2025-08-16 21:49:53 +0530

    Update AppRoutes.jsx with about

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -8,12 +8,14 @@ import ServiceProviderProfile from '../pages/ServiceProviderProfile';
 import ProviderHome from '../pages/ProviderHome'
 import FindServices from '../pages/FindServices'
 import QuickBookForm from '../pages/QuickBookForm'
+import About from '../pages/About'
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignupForm />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/About" element={<About />} />
       <Route path="/providerhome" element={<ProviderHome />} />
       <Route path="/findservices" element={<FindServices />} />
         <Route path="/quickbookform" element={<QuickBookForm />} />
