a8434b30ff89b28de7ab4514ff42d454d20e4f67
ksdfernando <ksachithradf@gmail.com>
2025-08-16 09:19:37 +0530

    Create ProviderHome UI

---
+++ frontend/src/pages/ProviderHome.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaTools, FaCalendarAlt, FaMoneyBillWave, FaUserCog, FaPlus } from "react-icons/fa";
+import "../ProviderHome.css";
+import { Link } from "react-router-dom";
+
+const ProviderHome = () => {
+  return (
+    <div className="provider-home">
+     
+     <h1>Provider Dashbord</h1>
+
+      {/* Dashboard Cards */}
+      <div className="cards-grid">
+        <div className="card">
+          <FaTools size={35} />
+          <h4>My Services</h4>
+        </div>
+        
+       <Link to="/creteservice" className="card-link">
+         <div className="card">
+         <FaPlus size={35} />
+         <h4>Add New Service</h4>
+         </div>
+       </Link>
+
+        <div className="card">
+          <FaCalendarAlt size={35} />
+          <h4>Bookings</h4>
+        </div>
+       
+        <div className="card">
+          <FaMoneyBillWave size={35} />
+          <h4>Earnings</h4>
+        </div>
+        <div className="card">
+          <FaUserCog size={35} />
+          <h4>Profile</h4>
+        </div>
+        
+      </div>
+
+      {/* Floating Action Button */}
+      <button className="fab">
+        <FaPlus size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default ProviderHome;
